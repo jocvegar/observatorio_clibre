ae1b9d608905f29bde48ba0d5bcf7497ef32ff87
Jose <jocvegar@gmail.com>
2021-04-29 06:30:14 -0600

    initial commit

---
+++ src/components/MapComponents/MapBox.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="container">
+    <div id="mapContainer"></div>
+  </div>
+</template>
+<script>
+import L from "leaflet";
+const accessToken =
+  "pk.eyJ1Ijoiam9jdmVnYXIiLCJhIjoiY2tvMWljN3R2MG01cTJucnRhajd3OXVvNSJ9.XVf_8jXk9CZg07XolcEZ3Q";
+const id = "mapbox/satellite-v9";
+
+export default {
+  name: "Map",
+  data() {
+    return {
+      center: [47.31322, -1.319482],
+    };
+  },
+  methods: {
+    setupLeafletMap: function() {
+      const mapDiv = L.map("mapContainer").setView(this.center, 13);
+      L.tileLayer(
+        `https://api.mapbox.com/styles/v1/${id}/tiles/{z}/{x}/{y}?access_token=${accessToken}`,
+        {
+          attribution:
+            'Map data (c) <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: "mapbox/streets-v11",
+          accessToken: accessToken,
+        }
+      ).addTo(mapDiv);
+    },
+  },
+  mounted() {
+    this.setupLeafletMap();
+  },
+};
+</script>
+
+<style scoped>
+#mapContainer {
+  width: 80vw;
+  height: 100vh;
+}
+</style>
---
+++ src/components/MapComponents/MapVisual2.vue
@@ -0,0 +1,65 @@
+<template>
+  <div style="height: 80vh">
+    <LMap :zoom="zoom" :center="center">
+      <LTileLayer :url="url"></LTileLayer>
+      <LMarker :lat-lng="[14.0849, -87.2077]">
+        <LPopup>300 Casos</LPopup>
+      </LMarker>
+      <LMarker :lat-lng="[15.5313, -88.1384]">
+        <LPopup>100 Casos</LPopup>
+        <LTooltip>Click más informacion</LTooltip>
+      </LMarker>
+    </LMap>
+  </div>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker, LPopup } from "vue2-leaflet";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup,
+  },
+  data() {
+    return {
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      zoom: 8,
+      center: [47.31322, -1.319482],
+      markerLatLng: [47.31322, -1.319482],
+    };
+  },
+};
+</script>
+
+<script>
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LPopup,
+  LTooltip,
+  LIcon,
+} from "vue2-leaflet";
+export default {
+  name: "Map",
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup,
+    LTooltip,
+    LIcon,
+  },
+  data() {
+    return {
+      url: "https://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      zoom: 8,
+      center: [14.805, -86.231],
+      bounds: null,
+    };
+  },
+};
+</script>
