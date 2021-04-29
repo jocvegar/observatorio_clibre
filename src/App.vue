46223c92806a161d688bf312c925755691360446
Jose <jocvegar@gmail.com>
2021-04-29 08:46:19 -0600

    finally adds nav

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,77 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app color="white" class="px-4">
       <div class="d-flex align-center">
-        <h2>Edwin</h2>
+        <v-img
+          alt="Cibre Logo"
+          class="shrink mr-2"
+          contain
+          src="@/assets/clibre_logo.jpg"
+          transition="scale-transition"
+          width="100"
+        />
+        <v-toolbar-title
+          class="shrink mt-1 hidden-sm-and-down blue--text text--darken-4 ml-1"
+        >
+          Observatorio CLibre
+        </v-toolbar-title>
       </div>
+
       <v-spacer></v-spacer>
+
+      <div class="hidden-sm-and-down">
+        <v-btn
+          v-for="(item, idx) in items"
+          :key="idx"
+          :to="item.to"
+          text
+          exact
+          link
+          id="no-background-hover"
+        >
+          <v-icon color="blue darken-4">{{ item.icon }}</v-icon>
+          <span class="mr-2 pl-2 blue--text text--darken-4">
+            {{ item.title }}
+          </span>
+        </v-btn>
+        <!-- <v-btn
+          href="https://github.com/vuetifyjs/vuetify/releases/latest"
+          target="_blank"
+          text
+        >
+          <span class="mr-2 blue--text text--darken-4">Nav 1</span>
+          <v-icon color="blue darken-4">mdi-open-in-new</v-icon>
+        </v-btn>
+        -->
+      </div>
+
+      <div class="hidden-sm-and-up">
+        <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </v-btn>
+      </div>
     </v-app-bar>
+
+    <v-navigation-drawer
+      app
+      v-model="rightDrawer"
+      :right="true"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item v-for="([icon, text], i) in sideItems" :key="i" link>
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
       <router-view />
     </v-main>
@@ -16,7 +82,29 @@
 export default {
   name: "App",
   data: () => ({
-    //
+    rightDrawer: false,
+    sideItems: [["mdi-home", "Home"]],
+    items: [
+      {
+        icon: "mdi-home-variant",
+        title: "Home",
+        to: "/",
+      },
+      {
+        icon: "mdi-cup-water",
+        title: "Ventas",
+        to: "/ventas",
+      },
+    ],
   }),
 };
 </script>
+
+<style>
+.v-navigation-drawer--temporary {
+  z-index: 1001 !important;
+}
+#no-background-hover::before {
+  background-color: transparent !important;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <!-- <MapVisual2></MapVisual2> -->
-    <MapBox></MapBox>
+    <MapVisual2></MapVisual2>
+    <!-- <MapBox></MapBox> -->
   </div>
 </template>
 
 <script>
-// import MapVisual2 from "@/components/MapComponents/MapVisual2";
-import MapBox from "@/components/MapComponents/MapBox";
+import MapVisual2 from "@/components/MapComponents/MapVisual2";
+// import MapBox from "@/components/MapComponents/MapBox";
 export default {
   name: "HelloWorld",
   components: {
-    // MapVisual2,
-    MapBox,
+    MapVisual2,
+    // MapBox,
   },
 };
 </script>
